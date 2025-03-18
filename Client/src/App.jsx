092ad12f7da19f3aaecbf2e3e54ff1341e29c159
Archisman03 <archismangoswami2004@gmail.com>
2025-03-18 10:24:04 +0530

    install react js and tailwind css and create the first template

---
+++ Client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div class='bg-slate-800 text-white'>
+      </div>
+    </>
+  )
+}
+
+export default App
